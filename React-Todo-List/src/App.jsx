f99b1dc2500d4a40ebc3e69eb9e782e3b5ed83d5
Harrisonk76 <165740611+Harrisonk76@users.noreply.github.com>
2024-09-28 16:18:07 -0700

    update

--- React-Todo-List/src/App.jsx
+++ React-Todo-List/src/App.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { useTodos } from './hooks/useTodos';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './styles/App.css';
 
+const App = () => {
+  const { todos, dispatch } = useTodos();
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoForm dispatch={dispatch} />
+      <TodoList todos={todos} dispatch={dispatch} />
+    </div>
+  );
+};
+
+export default App;
